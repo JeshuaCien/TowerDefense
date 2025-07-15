2e26fbfc981631c03e410589f762ea18af2e8930
Jeshua Cienfuegos Estrada <156126427+JeshuaCien@users.noreply.github.com>
2025-07-14 20:48:54 -0600

    HaH
    Vol

---
+++ Assets/Scripts/ClicksRaycast.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClicksRaycast : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask _raycastLayerMask;
+
+    [SerializeField]
+    private string _coinTagname = "Coin";
+
+    [SerializeField]
+    private float _rayDistance = 100f;
+
+    [SerializeField]
+    private UnityEvent<Transform> _onCoinCollected;
+
+    private bool _isActive = true;
+
+    public bool IsActive { set => _isActive = value; }
+
+    private void Update()
+    {
+        if (_isActive && Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, _rayDistance, _raycastLayerMask))
+            {
+                if (hit.collider.CompareTag(_coinTagname))
+                {
+                    Coin coin = hit.collider.GetComponent<Coin>();
+                    if (coin != null)
+                    {
+                        CoinCollected(coin);
+                    }
+                }
+            }
+        }
+    }
+
+    private void CoinCollected(Coin coin)
+    {
+        coin.Collect();
+        _onCoinCollected?.Invoke(coin.transform);
+    }
+}
---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent _onCoinCollected;
+
+    [SerializeField]
+    private Animator _animator;
+
+    [SerializeField]
+    private string _appearAnimationName = "CoinAppear";
+
+    [SerializeField]
+    private string _disappearAnimationName = "CoinDisAppear";
+
+    [SerializeField]
+
+    private float _SecondsToDisappear = 2f;
+
+    private Coroutine _appearCoroutine;
+
+    private void Onnable()
+    {
+        _appearCoroutine = StartCoroutine(Appear());
+    }
+
+    private IEnumerator Appear()
+    {
+        _animator.Play(_appearAnimationName);
+        yield return new WaitForSeconds(_SecondsToDisappear);
+        StartCoroutine(Disappear());
+    }
+
+    public void Collect()
+    {
+        Stop();
+        StartCoroutine(Disappear());
+    }
+
+    private IEnumerator Disappear()
+    {
+        _animator.Play(_disappearAnimationName);
+        yield return new WaitForSeconds(_animator.GetCurrentAnimatorStateInfo(0).length);
+        gameObject.SetActive(false);
+    }
+
+    private void Stop()
+    {
+        if (_appearCoroutine != null)
+        {
+            StopCoroutine(_appearCoroutine);
+            _appearCoroutine = null;
+        }
+    }
+}
