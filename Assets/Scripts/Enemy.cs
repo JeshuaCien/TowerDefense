9cf421febd9dcbb0b9718282ba22cf71d1252dec
Jeshua Cienfuegos Estrada <156126427+JeshuaCien@users.noreply.github.com>
2025-07-22 20:29:43 -0600

    Commit casi final
    Hello

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Enemy : MonoBehaviour
-{   [SerializeField]
+{ [SerializeField]
     private Animator _animator;
     [SerializeField]
     private string _targetTag = "Tower";
 
     [SerializeField]
+    private UnityEvent _onIntialized;
+    private Vector3 _targetPosition;
+
+    [SerializeField]
     private EnemyData _enemyData;
-    private Transform _target;
 
     private Health _targetHealth;
 
@@ -17,8 +21,9 @@ public class Enemy : MonoBehaviour
 
     private void OnEnable()
     {
+        _onIntialized?.Invoke();
         _isRunning = false;
-        GetTarget();
+        Invoke("GetTarget", 0.5f);
     }
 
     private void GetTarget()
@@ -26,7 +31,7 @@ public class Enemy : MonoBehaviour
         GameObject targetObject = GameObject.FindGameObjectWithTag(_targetTag);
         if (targetObject != null)
         {
-            _target = targetObject.transform;
+            _targetPosition = new Vector3(targetObject.transform.position.x, transform.position.y, targetObject.transform.position.z);
             _targetHealth = targetObject.GetComponent<Health>();
             _isRunning = true;
             _animator.Play(_enemyData._RunAnimationName);
@@ -37,9 +42,10 @@ public class Enemy : MonoBehaviour
     {
         if (_isRunning)
         {
-            transform.LookAt(_target);
-            transform.position = Vector3.MoveTowards(transform.position, _target.position, _enemyData._RunSpped * Time.deltaTime);
-            if (Vector3.Distance(transform.position, _target.position) <= _enemyData._AttackRange)
+            transform.LookAt(_targetPosition);
+            Vector3 movePosition = Vector3.MoveTowards(transform.position, _targetPosition, _enemyData._RunSpped * Time.deltaTime);
+            transform.position = movePosition;
+            if (Vector3.Distance(transform.position, _targetPosition) <= _enemyData._AttackRange)
             {
                 _isRunning = false;
                 StartCoroutine(Attack());
@@ -50,19 +56,38 @@ public class Enemy : MonoBehaviour
 
     private IEnumerator Attack()
     {
-        while (_target != null && _targetHealth.CurrentHealth > 0)
+        while (_targetHealth != null && _targetHealth.CurrentHealth > 0)
         {
             _animator.Play(_enemyData._AttackAnimationName, 0, 0f);
             yield return new WaitForSeconds(_enemyData._AttackDuration);
             _targetHealth.TakeDamage(_enemyData._AttackDamage);
             yield return new WaitForSeconds(_enemyData._AttackWaitTime);
         }
+        Win();
     }
 
+    private void Win()
+    {
+        _animator.Play(_enemyData._WinAnimationName);
+        _isRunning = false;
+    }
+
+    public void Die()
+    {
+        _isRunning = false;
+        StopAllCoroutines();
+        StartCoroutine(DieCoroutine());
+    }
+
+    private IEnumerator DieCoroutine()
+    {
+        _animator.Play(_enemyData._DeathAnimationName);
+        yield return new WaitForSeconds(_animator.GetCurrentAnimatorStateInfo(0).length);
+        gameObject.SetActive(false);
+    }
     private void OnDisable()
     {
         _isRunning = false;
-        _target = null;
         _targetHealth = null;
         StopAllCoroutines();
     }
