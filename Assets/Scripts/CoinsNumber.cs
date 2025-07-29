cfb1512a96898c20318f028f810c75188431c47c
Jeshua Cienfuegos Estrada <156126427+JeshuaCien@users.noreply.github.com>
2025-07-28 20:43:05 -0600

    Commit_Refreash
    Hello

--- Assets/Scripts/CoinsNumber.cs
+++ Assets/Scripts/CoinsNumber.cs
@@ -5,10 +5,16 @@ public class CoinsNumber : MonoBehaviour
 {
     [SerializeField]
     private int _coins;
-    public int Coins {get => _coins;}
+    public int Coins { get => _coins; }
 
     [SerializeField]
     private UnityEvent<int> _onCoinsUpdated;
+
+    [SerializeField]
+    private UnityEvent _onBuySucces;
+
+    [SerializeField]
+    private UnityEvent _OnbuyFail;
 
     public void AddCoins(int amount)
     {
@@ -16,7 +22,7 @@ public class CoinsNumber : MonoBehaviour
         SetCoins(_coins);
     }
 
-    public void SetCoins (int amount)
+    public void SetCoins(int amount)
     {
         _coins = amount;
         _onCoinsUpdated?.Invoke(_coins);
@@ -28,4 +34,20 @@ public class CoinsNumber : MonoBehaviour
         if (_coins < 0) _coins = 0;
         SetCoins(_coins);
     }
+
+    public bool BuyObject(int cost)
+    {
+        if (_coins >= cost)
+        {
+            SubstracCoins(cost);
+            _onBuySucces?.Invoke();
+            return true;
+        }
+        else
+        {
+            _OnbuyFail?.Invoke();
+            return false;
+        }
+    }
+    
 }
